63a5d277ca0952400d6be24bb2e81015584d24bd
sina nazemi <dmcsina2@gmail.com>
2023-10-11 16:03:01 +0330

    build domin and infra layar

---
+++ Sina.Mocut.Backend.Core/Entitis/Barber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sina.Mocut.Backend.Core.Entitis
+{
+    public class Barber
+    {
+        public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        public Barber(string? phoneNumber, string? fullName)
+        {
+            PhoneNumber = phoneNumber;
+            FullName = fullName;
+        }
+    }
+}
---
+++ Sina.Mocut.Backend.Core/Entitis/SystemManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sina.Mocut.Backend.Core.Entitis
+{
+    public class SystemManagement
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public SystemManagement(string password, string username)
+        {
+            Password = password;
+            Username = username;
+        }
+    }
+}
